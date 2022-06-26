97740fe1d36bf8afbaaedb6519374843772e767c
Stanislav-PSV <stanislav.psv521@gmail.com>
2022-06-27 02:11:20 +0300

    007

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const options = '?fields=name,capital,population,flags,languages';
+
+export function fetchCountries(name) {
+  const url = `${BASE_URL}${name}${options}`;
+  return fetch(url).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error(res);
+  });
+}
---
+++ src/getRefs.js
@@ -0,0 +1,8 @@
+export function allRefs() {
+    return {
+        inputEl: document.querySelector('#search-box'),
+        countryInfo: document.querySelector('.country-info'),
+        countryList: document.querySelector('.country-list')
+    }
+
+}
